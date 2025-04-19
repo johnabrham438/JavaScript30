e1e61d764885fac04245cc15230169aaf2f145f3
john abrham <yohansabrham43@gmail.com>
2025-04-19 05:11:04 -0800

    Day1 Finished

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,15 @@
+function playAudio (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+
+    setTimeout(() => key.classList.remove('playing'), 70);
+   
+}
+window.addEventListener("keydown", playAudio)
